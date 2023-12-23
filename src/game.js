c3dd59becfe055c861c87bcf6d2397827c5a2bf9
En Yu <enyuuu.chew@gmail.com>
2023-12-24 03:55:33 +0800

    updated main page

--- src/game.js
+++ src/game.js
@@ -8,5 +8,17 @@
  * ID and Name #3   : 
  * Contacts    #3   : 
 *****************************************************/
-
 // main code goes here
+
+// configure the game layout
+var config = {
+    width: 700,
+    height: 700,
+    backgroundColor: 0x000000,
+    scene: [BootScene, MenuScene],
+    pixelArt: true,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+}
+
+// create a game instance
+var game = new Phaser.Game(config);
